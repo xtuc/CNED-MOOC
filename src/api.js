a720c6b9119daa9aff0a50ce3320699fd47592bd
Sven SAULEAU <sven.sauleau@xtuc.fr>
2017-06-22 14:30:58 +0200

    feat(api): handle errors

--- src/api.js
+++ src/api.js
@@ -1,32 +1,40 @@
 import {log} from "./messages"
 
-function JSONRequest(url, onSuccess) {
+function JSONRequest(url, onSuccess, onError) {
 
   /**
    * Call the onSuccess cb with parsed data
    *
    * @param {string} data
    */
-  function success(data) {
-    return onSuccess(data.parse)
+  function complete(data) {
+    if (data.responseJSON.parse) {
+      return onSuccess(data.responseJSON.parse)
+    }
+
+    if (data.responseJSON.error && onError) {
+      return onError(data.error)
+    }
   }
 
   $.ajax({
     dataType: "json",
     url,
-    success,
+    complete,
     error: log
   })
 }
 
-export function getPage(page, cb) {
+export function getPage(page, onSuccess, onError) {
   const url = `/w/api.php?action=parse&page=${page}&format=json&redirects`
 
-  return JSONRequest(url, cb)
+  return JSONRequest(url, onSuccess, onError)
 }
 
 export function getPageNameFromUrl(url) {
-  const [,firstMatch] = /\/wiki\/(.*)\?.*/.exec(url)
+  const matches = /\/wiki\/(.*)\?.*/.exec(url)
 
-  return firstMatch
+  if (matches && matches[1]) {
+    return matches[1]
+  }
 }
