f2e8270af8a11ee29b9b6c09e8b86070602dd57d
Sven SAULEAU <sven.sauleau@xtuc.fr>
2017-07-19 12:54:06 +0200

    fix: object.assign polyfill

--- src/Polyfill/IE.js
+++ src/Polyfill/IE.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 // @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
 //
 // Production steps of ECMA-262, Edition 6, 22.1.2.1
@@ -77,6 +79,31 @@ if (!Array.from) {
       return A
     }
   }())
+}
+
+if (typeof Object.assign != 'function') {
+  Object.assign = function(target, varArgs) { // .length of function is 2
+    'use strict';
+    if (target == null) { // TypeError if undefined or null
+      throw new TypeError('Cannot convert undefined or null to object');
+    }
+
+    var to = Object(target);
+
+    for (var index = 1; index < arguments.length; index++) {
+      var nextSource = arguments[index];
+
+      if (nextSource != null) { // Skip over if undefined or null
+        for (var nextKey in nextSource) {
+          // Avoid bugs when hasOwnProperty is shadowed
+          if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+            to[nextKey] = nextSource[nextKey];
+          }
+        }
+      }
+    }
+    return to;
+  };
 }
 
 if (!console.debug)
